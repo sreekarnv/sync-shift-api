2200e565576b5fef1afaf3e9f78095589a0a9db5
Sreekar <sreekarnv1@gmail.com>
2023-05-01 14:16:17 +0530

    Add endpoint to fetch loggedin users booked facility slots

--- src/main/java/com/example/timingconsensusscheduler/controllers/UserController.java
+++ src/main/java/com/example/timingconsensusscheduler/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.example.timingconsensusscheduler.controllers;
 
 import com.example.timingconsensusscheduler.dto.UserBaseDto;
+import com.example.timingconsensusscheduler.entity.FacilitySlot;
+import com.example.timingconsensusscheduler.entity.User;
+import com.example.timingconsensusscheduler.services.FacilitySlotService;
 import com.example.timingconsensusscheduler.services.UserService;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
@@ -13,6 +18,7 @@ import java.util.*;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
+    private final FacilitySlotService facilitySlotService;
 
     @GetMapping("/")
     public ResponseEntity<List<UserBaseDto>> getMembers() {
@@ -25,5 +31,13 @@ public class UserController {
     @GetMapping("/health")
     public ResponseEntity<String> getString() {
         return ResponseEntity.status(HttpStatus.OK).body("Yo!!");
+    }
+
+    @GetMapping("/slots")
+    public ResponseEntity<List<FacilitySlot>> getLoggedInUserSlots() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        var user = (User) auth.getPrincipal();
+        var slots = facilitySlotService.findAllByUser(user);
+        return ResponseEntity.status(HttpStatus.OK).body(slots);
     }
 }
